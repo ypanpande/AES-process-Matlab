ebdfe2219bb108e5270298555808a71bb0c032d4
Yu Pan <ypanbx02@gmail.com>
2019-05-31 22:38:25 +0200

    table gui done

--- private/MakeTableGUI_Freq.m
+++ private/MakeTableGUI_Freq.m
@@ -15,3 +15,30 @@ empty = [{' '},{' '},{' '},{' '},{' '}];% empty row
 t = uitable(handles.fig,'Data',[addToRow_Freq; empty],    'ColumnWidth',{200,50,80,80,80},'ColumnName',{'','ch','fre_peak(kHz)','fre_centroid(kHz)','fre_wpeak(kHz)'},...
     'Position',[110 20 800 800]);
 
+set(t,'ColumnEditable',[logical(0),logical(0), logical(3:5)]);
+%scroll
+
+    function DispTableCallback(varargin)
+        t.Data = [t.Data;addToRow_Freq; empty]; % appending 1 new data
+        
+        %scroll to bottom
+        jScrollpane = findjobj(t);                % get the handle of the table
+        scrollMax = jScrollpane.getVerticalScrollBar.getMaximum;  % get the end position of the scroll
+        jScrollpane.getVerticalScrollBar.setValue(scrollMax);     % set scroll position to the end
+    end
+
+    function saveCallback(varargin)
+        Data=get(t,'Data');
+        ColumnName=get(t,'ColumnName');
+        
+        A = [ColumnName' ; Data];
+        
+        [file,path] = uiputfile('file.xls','Save Frequency table');
+        
+        ff = fullfile(path, file);
+        xlswrite(ff,A)
+        msgbox('done!')
+    end
+
+
+end
