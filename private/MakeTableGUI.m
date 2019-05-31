177bb63eda4bb2aad54f651e5d7c86dd4a4449fe
Yu Pan <ypanbx02@gmail.com>
2019-05-31 20:36:45 +0200

    figure set

--- private/MakeTableGUI.m
+++ private/MakeTableGUI.m
@@ -15,6 +15,13 @@ t = uitable(handles.fig,'Data',addToRow,'ColumnWidth',{200,70,70,70,70,70,70},'C
     'Position',[110 20 700 560]);
 set(t,'ColumnEditable',[logical(0), logical(1:5)])
 
+    function DispTableCallback(varargin)
+        t.Data = [t.Data;addToRow];
+        %scroll to bottom
+        jScrollpane = findjobj(t);                % get the handle of the table
+        scrollMax = jScrollpane.getVerticalScrollBar.getMaximum;  % get the end position of the scroll
+        jScrollpane.getVerticalScrollBar.setValue(scrollMax);     % set scroll position to the end
+    end
 
     function saveCallback(varargin)
         Data=get(t,'Data');
--- private/MakeTableGUI_Freq.m
+++ private/MakeTableGUI_Freq.m
@@ -3,13 +3,7 @@ function MakeTableGUI_Freq
 
 global addToRow_Freq;
 
-%// Create figure and uielements
-handles.fig = figure('Position',[10000 10000 700 830], 'Name', 'save table of frequency','NumberTitle','off');
-movegui(handles.fig,'center');
-handles.DispButton = uicontrol('Style','Pushbutton','Position',[20 120 80 40],'String','addFreq','Callback',@DispTableCallback);
-handles.SaveButton = uicontrol('Style','Pushbutton','Position',[20 80 70 40],'String','exportFreq','Callback',@saveCallback);
 
-empty = [{' '},{' '},{' '},{' '},{' '}];% empty row
 
 %// Place table in GUI
 t = uitable(handles.fig,'Data',[addToRow_Freq; empty],    'ColumnWidth',{200,50,80,80,80},'ColumnName',{'','ch','fre_peak(kHz)','fre_centroid(kHz)','fre_wpeak(kHz)'},...
