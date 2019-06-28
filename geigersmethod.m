bead27e76e7feafa3a5b3a40d275f3a17f9c4f3f
Yu Pan <ypanbx02@gmail.com>
2019-06-28 13:49:14 +0200

    loc methods

---
+++ geigersmethod.m
@@ -0,0 +1,130 @@
+function geigersmethod()
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+clear; clc; close all;
+
+format long
+
+% Determining the predicted arrival time using the Geiger's Method
+%% set parameters
+% sensor coordinates (xi, yi)
+% sx=[5, 0, 505, 550];
+% sy=[1555,0, 1482.5,82.5];
+sx=[5, 0, 505, 550, -1650, -1650];
+sy=[1555,0, 1482.5,82.5,1552.5, -2.5];
+
+%velocity component
+v0 = [4000, 2500];
+
+
+
+
+N = length(sx); % number of sensor
+M = 3; % number of model parameter [dx,dy,dt]
+
+% stop criterion res < epsilon = 1.0e-2;
+epsilon=1.0e-2;
+
+% save the result
+eventMatrix = {}; 
+header = [{'event name'},{'x0 (mm)'},{'y0 (mm)'},{'t0 (ms)'},{'RMS res time'},{'average res time (ms)'},{'tmin (ms)'},{'CH'}];
+
+%% Determining the predicted arrival time using the Geiger's Method
+
+% choose the file for analysing
+[name,curvePath] = uigetfile( ...
+    {'*.txt; *.xls; *.csv','Text file(*.txt, *.xls, *.csv)';...
+    '*.*',  'All Files (*.*)'}, ...
+    'Pick a file', ...
+    'MultiSelect', 'on');
+
+if ~isempty(name) & ~isa(name,'double')% only if choose one or more files
+    filename = cellstr(name);    
+end
+
+filenumber = length(filename);
+
+for filesloop = 1:filenumber
+  
+[data, txt, raw] = xlsread(fullfile(curvePath,filename{filesloop}));
+[row_data, col_data] = size(data);
+
+eventMatrix_file = {};
+for eventnum = 1:row_data
+    % observed arrive time
+    dobs = data(eventnum,:)';
+    % % inital guess 
+    [t_ini,index] = min(dobs);
+    mest = [sx(index)+randi(100),sy(index)+randi(100),t_ini-0.05];
+   
+    %div(1)=100;
+    %Formulating the data kernel matrix and estimating the predicted models
+    for iter=1:100 %for 10 iterations (termination criteria)
+        
+        % formulating data kernel
+        G = spalloc(N,M,N*M); %N- total num of data,2*Ne*Ns; M is total num of model
+        %parameters, 4*Ne
+        dpre = zeros(N,1); %allocating space for predicted data matrix
+        for i = 1:N % loop over stations
+            
+            dx = mest(1)-sx(i); % x- component of displacement obtained using the initial guess
+            dy = mest(2)-sy(i); % y- component of displacement
+            
+            r = sqrt( dx^2 + dy^2 ); % source-receiver distance for each iteration
+            
+            weightv = abs(floor([dx, dy]/10)); % calculate the velocity
+            vel = sum(v0.*weightv)/sum(weightv);
+            
+            dpre(i)=r/vel+mest(3); %predicted signal arrival time
+            
+            %data kernel matrix
+            G(i,1) = dx/(r*vel); % first column of data kernel matrix
+            G(i,2) = dy/(r*vel); % second column of data kernel matrix
+            G(i,3) = 1; % third column of data kernel matrix
+            
+            
+        end
+        
+        % solve with dampled least squares
+        dd = dobs-dpre;
+        dm = (G'*G)\(G'*dd);
+        %dm=bicg(@dlsfun,G'*dd,1e-2); %solving using the biconjugate method
+        %solving the damped least square equation G'dd = [ G'G + epsilon* I] dm
+        % We use biconjugate method to reduce the computational cost (see for the dlsfun at the bottom)
+        mest = mest+dm; %updated model parameter
+        
+        res = sqrt((dd'*dd)/(N-M));
+%         div(iter+1) = res;
+        
+        if res <= epsilon
+            break
+        end
+        
+%          if div(iter) < res
+%              mest = mest - dm;
+%              res = div(iter);
+%              break
+%          end
+        
+    end
+
+    aver_dd = sqrt((N-M)*res^2/N);
+    addToRow = [{txt{eventnum+1}},{mest(1)},{mest(2)},{mest(3)},{res},{aver_dd},{t_ini},{index-1}];
+    eventMatrix_file = [eventMatrix_file;addToRow];
+end
+    eventMatrix = [eventMatrix;eventMatrix_file];
+end
+result_matrix = [header;eventMatrix];
+
+%% save the result
+% save the data in the certain files
+        [resultfile,saveresultpath] = uiputfile( ...
+    {'*.txt; *.xls; *.csv','Text file(*.txt, *.xls, *.csv)';...
+    '*.*',  'All Files (*.*)'},'Save file of geigersmethod result', 'geigersmethod result.xls');
+        ffresult = fullfile(saveresultpath, resultfile);
+        xlswrite(ffresult,result_matrix)
+        
+msgbox('done!')
+
+end
+
---
+++ sequentialmethod.m
@@ -0,0 +1,128 @@
+%function sequentialmethod()
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+clear; close all;
+% determining the predicted arrival time using the sequential searching algorithms
+format long
+%% set the parameters
+% sensor coordinates (xi, yi)
+% sx=[5, 0, 505, 550];
+% sy=[1555,0, 1482.5,82.5];
+sx=[5, 0, 505, 550, -1650, -1650];
+sy=[1555,0, 1482.5,82.5,1552.5, -2.5];
+
+%velocity component
+v0 = [4000, 2500];
+
+
+N = length(sx); % number of sensor
+M = 3; % number of model parameter [dx,dy,dt]
+
+% save the result
+eventMatrix = {}; 
+header = [{'event name'},{'x0 (mm)'},{'y0 (mm)'},{'t0 (ms)'},{'RMS res time'},{'average res time (ms)'},{'tmin (ms)'},{'CH'}];
+
+%% determining the predicted arrival time using the sequential searching algorithms
+
+
+% choose the file for analysing
+[name,curvePath] = uigetfile( ...
+    {'*.txt; *.xls; *.csv','Text file(*.txt, *.xls, *.csv)';...
+    '*.*',  'All Files (*.*)'}, ...
+    'Pick a file', ...
+    'MultiSelect', 'on');
+
+if ~isempty(name) & ~isa(name,'double')% only if choose one or more files
+    filename = cellstr(name);    
+end
+
+filenumber = length(filename);
+
+for filesloop = 1:filenumber
+  
+[data, txt, raw] = xlsread(fullfile(curvePath,filename{filesloop}));
+[row_data, col_data] = size(data);
+
+eventMatrix_file = {}; 
+for eventnum = 1:row_data
+    % observed arrive time
+    dobs = data(eventnum,:);
+    % sensor of the first arrival
+    [t_ini,index] = min(dobs);
+    
+     %searching area from (-2000,2000) to (1200,-400) and the block is 20mm*20mm
+      block = 10;
+% %       areatop = [-2000,2000]; areabottom = [1200,-400];
+    switch index
+        case 1
+           areatop = [-840,2000]; areabottom = [260,760]; 
+        case 2
+            areatop = [-840,780]; areabottom =  [280,-400];
+        case 3
+            areatop = [240,2000]; areabottom = [1200,760];
+        case 4
+            areatop = [260,780]; areabottom = [1200,-400];
+        case 5
+            areatop = [-2000,2000]; areabottom = [-820,760];
+        case 6
+            areatop = [-2000,780]; areabottom = [-820,-400];
+    end
+
+    hornum = round((areabottom(1) - areatop(1))/block);
+    vernum = round((areatop(2) - areabottom(2))/block);
+    %rmsdd = spalloc(hornum,vernum,hornum*vernum);
+    
+    for i=1:vernum 
+    for j = 1:hornum
+        x = areatop(1)+ block/2 + (j-1)*block;
+        y = areabottom(2)+ block/2 + (i-1)*block;
+        a = [x,y];
+        for sensornum = 1:N % loop over sensors
+            dx = a(1)-sx(sensornum);
+            dy = a(2)-sy(sensornum);
+            % distance to sensors
+            distance(sensornum) = sqrt(dx^2 + dy^2);
+            % velocity to sensors % calculate the velocity
+            weightv = abs(floor([dx, dy]/10));
+            vel(sensornum) = sum(v0.*weightv)/sum(weightv);
+            % time to sensors
+            dcal(sensornum) = distance(sensornum)/vel(sensornum);           
+        end
+        
+        % calculate the time difference between calculation and observation using r_i = (t_oi - sum(t_oi)/n) - (t_cali - sum(t_cali)/n)
+        for sensornum2 = 1:N % loop over sensors
+        
+        dd(sensornum2) = (dobs(sensornum2) - sum(dobs)/N) - (dcal(sensornum2) - sum(dcal)/N);
+        end
+        
+        % calculate the event rms 
+
+        rmsdd(j,i) = sqrt((dd*dd')/(N-M));
+        t0(j,i) = sum(dobs)/N - sum(dcal)/N;
+    end
+    end
+    % find the index and value of the mininum rms of delta time
+    vmin = min(rmsdd(:));
+    [row_min,col_min] = find(rmsdd == vmin);
+    % calculate the position
+    xmin = areatop(1) + block/2 + (row_min(1)-1)*block;
+    ymin = areabottom(2) + block/2 + (col_min(1)-1)*block;
+    t0min = t0(row_min(1),col_min(1));
+    aver_dd = sqrt((N-M)*vmin^2/N);
+    addToRow = [{txt{eventnum+1}},{xmin},{ymin},{t0min},{vmin},{aver_dd},{t_ini},{index-1}];
+    eventMatrix_file = [eventMatrix_file;addToRow];
+end
+    eventMatrix = [eventMatrix;eventMatrix_file];
+end
+result_matrix = [header;eventMatrix];
+%% save the result
+% save the data in the certain files
+        [resultfile,saveresultpath] = uiputfile( ...
+    {'*.txt; *.xls; *.csv','Text file(*.txt, *.xls, *.csv)';...
+    '*.*',  'All Files (*.*)'},'Save file of sequential method result', 'sequential method result.xls');
+        ffresult = fullfile(saveresultpath, resultfile);
+        xlswrite(ffresult,result_matrix)
+
+msgbox('done!')
+
+%end
