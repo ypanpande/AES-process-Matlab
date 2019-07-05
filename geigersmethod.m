b861daa65ede30fdbf66620f2b7b26877c088cd5
Yu Pan <ypanbx02@gmail.com>
2019-07-05 19:52:22 +0200

    geiger init points

--- geigersmethod.m
+++ geigersmethod.m
@@ -31,39 +31,29 @@ header = [{'event name'},{'x0 (mm)'},{'y0 (mm)'},{'t0 (ms)'},{'RMS res time'},{'
 
 %% Determining the predicted arrival time using the Geiger's Method
 
+% choose the file for analysing
+[name,curvePath] = uigetfile( ...
+    {'*.txt; *.xls; *.csv','Text file(*.txt, *.xls, *.csv)';...
+    '*.*',  'All Files (*.*)'}, ...
+    'Pick a file', ...
+    'MultiSelect', 'on');
+
+if ~isempty(name) & ~isa(name,'double')% only if choose one or more files
+    filename = cellstr(name);    
+end
+
+filenumber = length(filename);
+
+for filesloop = 1:filenumber
+  
+[data, txt, raw] = xlsread(fullfile(curvePath,filename{filesloop}));
+[row_data, col_data] = size(data);
 
 eventMatrix_file = {};
 for eventnum = 1:row_data
     % observed arrive time
     dobs = data(eventnum,:)';
-    % % inital guess 
-    [t_ini,index] = min(dobs);
-    mest = [sx(index)+randi(100),sy(index)+randi(100),t_ini-0.05];
-   
-    %div(1)=100;
-    %Formulating the data kernel matrix and estimating the predicted models
-    for iter=1:100 %for 10 iterations (termination criteria)
-        
-        % formulating data kernel
-        G = spalloc(N,M,N*M); %N- total num of data,2*Ne*Ns; M is total num of model
-        %parameters, 4*Ne
-        dpre = zeros(N,1); %allocating space for predicted data matrix
-        for i = 1:N % loop over stations
-            
-            dx = mest(1)-sx(i); % x- component of displacement obtained using the initial guess
-            dy = mest(2)-sy(i); % y- component of displacement
-            
-            r = sqrt( dx^2 + dy^2 ); % source-receiver distance for each iteration
-            
-            weightv = abs(floor([dx, dy]/10)); % calculate the velocity
-            vel = sum(v0.*weightv)/sum(weightv);
-            
-            dpre(i)=r/vel+mest(3); %predicted signal arrival time
-            
-            %data kernel matrix
-            G(i,1) = dx/(r*vel); % first column of data kernel matrix
-            G(i,2) = dy/(r*vel); % second column of data kernel matrix
-            G(i,3) = 1; % third column of data kernel matrix
+    
             
             
         end
