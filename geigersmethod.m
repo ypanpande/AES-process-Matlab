88b6458a64bc3e7a40798cbfdf696a980cb0a2c9
Yu Pan <ypanbx02@gmail.com>
2019-07-09 11:53:15 +0200

    geiger res

--- geigersmethod.m
+++ geigersmethod.m
@@ -53,7 +53,34 @@ eventMatrix_file = {};
 for eventnum = 1:row_data
     % observed arrive time
     dobs = data(eventnum,:)';
-    
+    % % inital guess 
+    [t_ini,index] = min(dobs);
+    mest = [sx(index)+randi(100),sy(index)+randi(100),t_ini-0.05];
+   
+    %div(1)=100;
+    %Formulating the data kernel matrix and estimating the predicted models
+    for iter=1:100 %for 10 iterations (termination criteria)
+        
+        % formulating data kernel
+        G = spalloc(N,M,N*M); %N- total num of data,2*Ne*Ns; M is total num of model
+        %parameters, 4*Ne
+        dpre = zeros(N,1); %allocating space for predicted data matrix
+        for i = 1:N % loop over stations
+            
+            dx = mest(1)-sx(i); % x- component of displacement obtained using the initial guess
+            dy = mest(2)-sy(i); % y- component of displacement
+            
+            r = sqrt( dx^2 + dy^2 ); % source-receiver distance for each iteration
+            
+            weightv = abs(floor([dx, dy]/10)); % calculate the velocity
+            vel = sum(v0.*weightv)/sum(weightv);
+            
+            dpre(i)=r/vel+mest(3); %predicted signal arrival time
+            
+            %data kernel matrix
+            G(i,1) = dx/(r*vel); % first column of data kernel matrix
+            G(i,2) = dy/(r*vel); % second column of data kernel matrix
+            G(i,3) = 1; % third column of data kernel matrix
             
             
         end
@@ -66,21 +93,7 @@ for eventnum = 1:row_data
         % We use biconjugate method to reduce the computational cost (see for the dlsfun at the bottom)
         mest = mest+dm; %updated model parameter
         
-        res = sqrt((dd'*dd)/(N-M));
-%         div(iter+1) = res;
-        
-        if res <= epsilon
-            break
-        end
-        
-%          if div(iter) < res
-%              mest = mest - dm;
-%              res = div(iter);
-%              break
-%          end
-        
-    end
-
+       
     aver_dd = sqrt((N-M)*res^2/N);
     addToRow = [{txt{eventnum+1}},{mest(1)},{mest(2)},{mest(3)},{res},{aver_dd},{t_ini},{index-1}];
     eventMatrix_file = [eventMatrix_file;addToRow];
