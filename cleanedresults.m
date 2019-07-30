bbfda953fa305722cf1f312f40a902ab389e321d
Yu Pan <ypanbx02@gmail.com>
2019-07-30 14:05:28 +0200

    threshold

--- cleanedresults.m
+++ cleanedresults.m
@@ -1,23 +1,6 @@
 % function cleanedresults()
 
 clear; clc; close all;
-% set parameters
-colres = 4; % the column number of rms in the dataset
-maxres = 0.05; % the threshold of rms 
-
-% choose the file for analysing
-[name,curvePath] = uigetfile( ...
-    {'*.txt; *.xls; *.csv','Text file(*.txt, *.xls, *.csv)';...
-    '*.*',  'All Files (*.*)'}, ...
-    'Pick a file');
-
-if ~isempty(name) & ~isa(name,'double')% only if choose one or more files
-    filename = cellstr(name);    
-end
-
-
-[data, txt, raw] = xlsread(fullfile(curvePath,filename{1}));
-[row_data, col_data] = size(data);
 
 
 % delete the rows with empty cells and cell value larger than threshold
