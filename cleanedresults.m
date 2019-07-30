fd48eee9650a4f54767fa9642b5b29ea70d2526b
Yu Pan <ypanbx02@gmail.com>
2019-07-30 18:06:37 +0200

    filter

--- cleanedresults.m
+++ cleanedresults.m
@@ -20,17 +20,16 @@ end
 [row_data, col_data] = size(data);
 
 
+% delete the rows with empty cells and cell value larger than threshold
+new_raw(1,:) = raw(1,:);
+for i = 1: row_data
+    if ~isempty(data(i,colres)) 
+        if data(i,colres) <= maxres
+            new_raw(i+1,:) = raw(i+1,:);
+        end
+    end
+end
 
 
-output = new_raw(~all(cellfun('isempty',new_raw),2),:);
-% output = new_raw(~cellfun('isempty', new_raw'));
-% save the data in the certain files
-        [resultfile,saveresultpath] = uiputfile( ...
-    {'*.txt; *.xls; *.csv','Text file(*.txt, *.xls, *.csv)';...
-    '*.*',  'All Files (*.*)'},'Save file of cleaned result', 'cleaned result.xls');
-        ffresult = fullfile(saveresultpath, resultfile);
-        xlswrite(ffresult,output)
-
-msgbox('done!')
 
 %end
