54390e55b8eae71b08f14eb3afd75098c49d8417
Yu Pan <ypanbx02@gmail.com>
2019-07-30 19:07:13 +0200

    output

--- cleanedresults.m
+++ cleanedresults.m
@@ -30,6 +30,15 @@ for i = 1: row_data
     end
 end
 
+output = new_raw(~all(cellfun('isempty',new_raw),2),:);
+% output = new_raw(~cellfun('isempty', new_raw'));
+% save the data in the certain files
+        [resultfile,saveresultpath] = uiputfile( ...
+    {'*.txt; *.xls; *.csv','Text file(*.txt, *.xls, *.csv)';...
+    '*.*',  'All Files (*.*)'},'Save file of cleaned result', 'cleaned result.xls');
+        ffresult = fullfile(saveresultpath, resultfile);
+        xlswrite(ffresult,output)
 
+msgbox('done!')
 
 %end
