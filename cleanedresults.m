7243b34b26b67550d18994a609e4722a0658e981
Yu Pan <ypanbx02@gmail.com>
2019-07-30 16:06:04 +0200

    input

--- cleanedresults.m
+++ cleanedresults.m
@@ -1,17 +1,26 @@
 % function cleanedresults()
 
 clear; clc; close all;
+% set parameters
+colres = 4; % the column number of rms in the dataset
+maxres = 0.05; % the threshold of rms 
 
+% choose the file for analysing
+[name,curvePath] = uigetfile( ...
+    {'*.txt; *.xls; *.csv','Text file(*.txt, *.xls, *.csv)';...
+    '*.*',  'All Files (*.*)'}, ...
+    'Pick a file');
 
-% delete the rows with empty cells and cell value larger than threshold
-new_raw(1,:) = raw(1,:);
-for i = 1: row_data
-    if ~isempty(data(i,colres)) 
-        if data(i,colres) <= maxres
-            new_raw(i+1,:) = raw(i+1,:);
-        end
-    end
+if ~isempty(name) & ~isa(name,'double')% only if choose one or more files
+    filename = cellstr(name);    
 end
+
+
+[data, txt, raw] = xlsread(fullfile(curvePath,filename{1}));
+[row_data, col_data] = size(data);
+
+
+
 
 output = new_raw(~all(cellfun('isempty',new_raw),2),:);
 % output = new_raw(~cellfun('isempty', new_raw'));
